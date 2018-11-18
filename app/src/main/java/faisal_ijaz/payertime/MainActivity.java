a216e53bc5aea46a616336373adcb82e6d195ae5
Developerzone01 <fijaz418@gmail.com>
2018-11-19 02:45:32 +0500

    Initial commit

---
+++ app/src/main/java/faisal_ijaz/payertime/MainActivity.java
@@ -0,0 +1,23 @@
+package faisal_ijaz.payertime;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void location(View view) {
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
+    }
+}
+
+
